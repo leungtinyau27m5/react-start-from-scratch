043684b95dd694b6cf16ce035830b4db34c4ae07
leungtinyau27m5 <leungtinyau27m5@gmail.com>
2022-11-01 20:59:41 +0800

    first tutorial init

---
+++ react-from-scratch/webpack.config.ts
@@ -0,0 +1,42 @@
+import path from "path";
+import { Configuration } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const config = (): Configuration => {
+  return {
+    mode: "production",
+    entry: path.resolve(__dirname, "./src/index.tsx"),
+    output: {
+      path: path.resolve(__dirname, "./dist/"),
+      filename: "[name].js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/i,
+          exclude: [/node_modules/, /\.(spec|test).(ts|js)x?$/i],
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+            cacheCompression: false,
+            compact: true,
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+      alias: {
+        src: path.resolve(__dirname, "./src/"),
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "index.html",
+        inject: true,
+      }),
+    ],
+  };
+};
+
+export default config;
